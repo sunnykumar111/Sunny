a2bd672b01255d8e46059477cc7eec6027ee2e10
sunnykumar111 <148511397+sunnykumar111@users.noreply.github.com>
2025-03-20 22:12:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import requests
-import time
 from flask import Flask, jsonify
 import requests
 import threading
@@ -44,27 +42,3 @@ def trigger_message():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
-# Replace with your Bot Token and Group Chat ID
-BOT_TOKEN = '7474354923:AAFK9TeP53vQ6SzKBCJ7MU3OOocEuLBsT88'
-GROUP_CHAT_ID = '-4765614301'
-MESSAGE = 'Hello from your bot!'
-
-# Telegram API URL
-BASE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-
-def send_message():
-    payload = {
-        'chat_id': GROUP_CHAT_ID,
-        'text': MESSAGE
-    }
-    response = requests.post(BASE_URL, data=payload)
-    if response.status_code == 200:
-        print('Message sent successfully!')
-    else:
-        print(f'Failed to send message: {response.text}')
-
-if __name__ == "__main__":
-    while True:
-        send_message()
-        time.sleep(120)  # Wait for 120 seconds before sending the next message
